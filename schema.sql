00d84a882c40902d165970232db8d3cab034b234
Mikko Kilpeläinen <mikko.s.kilpelainen@gmail.com>
2020-10-13 20:02:40 +0300

    heroku second try

--- schema.sql
+++ schema.sql
@@ -1,21 +1,16 @@
--- auto-generated definition
+
 create table restaurant_images
 (
-    id            serial not null
-        constraint pictures_pk
-            primary key,
+    id            serial primary key,
     restaurant_id integer,
     data          bytea
 );
 
 
 
--- auto-generated definition
 create table reviews
 (
-    id            serial not null
-        constraint reviews_pk
-            primary key,
+    id            serial primary key,
     review        text,
     restaurant_id integer,
     grade         integer,
@@ -24,39 +19,27 @@ create table reviews
 
 
 
--- auto-generated definition
 create table users
 (
-    id       integer default nextval('projekti.users_id_seq'::regclass),
+    id       serial primary key,
     username text,
     password text,
     auth     text
 );
 
 
-create unique index users_username_uindex
-    on users (username);
-
--- auto-generated definition
 create table user_images
 (
-    id      serial not null
-        constraint user_images_pk
-            primary key,
+    id      serial primary key,
     user_id integer,
     data    bytea
 );
 
 
-create unique index user_images_user_id_uindex
-    on user_images (user_id);
 
--- auto-generated definition
 create table restaurants
 (
-    id      serial not null
-        constraint restaurants_pk
-            primary key,
+    id      serial primary key,
     name    text,
     address text,
     intro   text,
@@ -65,6 +48,4 @@ create table restaurants
 );
 
 
-create unique index restaurants_name_uindex
-    on restaurants (name);
 
