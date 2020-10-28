69f0279ff7177b0de847462346600853fa115b81
Mikko Kilpeläinen <mikko.s.kilpelainen@gmail.com>
2020-10-28 18:59:12 +0200

    Made a run.py file and added load_dotenv to make local version work on windows

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 from os import getenv
 from flask_googlemaps import GoogleMaps
 
+load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__) # tama on app
+# app = Flask("__main__") # tama on app
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -14,23 +17,30 @@ db = SQLAlchemy(app)
 GoogleMaps(app, key=getenv("GOOGLE_API_KEY"))
 
 
-def main():
-    register_blueprints()
-    app.run()
+# def main():
+#     register_blueprints()
+#     app.run()
 
 
-def register_blueprints():
-    from routes import home_routes
-    from routes import account_routes
-    from routes import restaraunt_routes
-    from routes import review_routes
-    app.register_blueprint(home_routes.blueprint)
-    app.register_blueprint(account_routes.blueprint)
-    app.register_blueprint(restaraunt_routes.blueprint)
-    app.register_blueprint(review_routes.blueprint)
+# def register_blueprints():
+from routes import home_routes
+from routes import account_routes
+from routes import restaraunt_routes
+from routes import review_routes
+app.register_blueprint(home_routes.blueprint)
+app.register_blueprint(account_routes.blueprint)
+app.register_blueprint(restaraunt_routes.blueprint)
+app.register_blueprint(review_routes.blueprint)
 
 
 
-if __name__ == "__main__":
-    main()
+print(app.secret_key)
 
+# if __name__ == "__main__":
+#     print()
+#     print('hei')
+#     main()
+
+
+# if __name__ == "app":
+#     main()
